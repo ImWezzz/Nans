444a45155bab7dbc1a15e0ef99f4efb94a1e81e4
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-03-06 06:45:58 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord, asyncio, os
+from discord.ext import commands
+from midb import Database
+
+db = Database(path="./db", tables=["main", "guilds", "users"])
+
+async def get_prefix(client, message: discord.Message):
+    if not message.guild:
+        return '-'
+    return db.get(f"{message.guild.id}.prefix", "guilds") or "-" 
+
+bot = commands.Bot(
+    case_insensitive=True,
+    command_prefix=get_prefix,
+    owner_ids=[759233882926350346],
+    strip_after_prefix=True,
+    intents=discord.Intents.all(),
+    activity=discord.Activity(type=discord.ActivityType.watching, name="-help")
+)
+
+async def main():
+    for file in os.listdir('./cogs'):
+        if file.endswith('.py'):
+            await bot.load_extension(f'cogs.{file[:-3]}')
+    #await bot.load_extension('jishaku')
+    async with bot:
+        await bot.start("os.getenv('TOKEN')")
+    
+if __name__ == "__main__":
+    asyncio.run(main())
