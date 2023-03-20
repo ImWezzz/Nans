7a1e4da8a7a949bdc55d9ec304b751ca19d7463c
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-03-20 02:02:46 -0300

    Update Util.py

--- cogs/Util.py
+++ cogs/Util.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from main import db
+from utils.views import DropdownView
 import discord
 import sys
 import datetime
@@ -8,10 +9,16 @@ class Util(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
     # Help
-    @commands.hybrid_command(name="h")
-    async def help(self, ctx: commands.Context, *, query: str = None):
-        """Show this message"""
-        await ctx.send_help(query)
+    @commands.hybrid_command(name="help")
+    async def help(self, ctx: commands.Context):
+        """Commands list"""
+
+        embed = discord.Embed(colour=1399944)
+        embed.set_thumbnail(url=self.bot.user.display_avatar)
+        embed.set_author(name="Commands list")
+        embed.description = f"**Hi! If you want to see my list of commands use the menu below** \n\n **Total commands: {len(self.bot.commands)}** \n\n [Invite](https://dsc.gg/willow-bot) • [Support](https://discord.gg/ynNWwPRgv2) • [Website](https://willowbot.munlai.fun)"
+        miview = DropdownView()
+        await ctx.send(embed=embed, view=miview)
 
     # Reverse
     @commands.hybrid_command(name="reverse")
@@ -19,7 +26,11 @@ class Util(commands.Cog):
         """Reverse a text"""
         await ctx.send(content=text[::-1])
 
-    
+    # Say
+    @commands.hybrid_command(namd="say")
+    async def say(self, ctx: commands.Context, *, text: str):
+        """I repeat your message"""
+        await ctx.send(content=text)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Util(bot))
