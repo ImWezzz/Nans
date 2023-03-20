7814dd388f0f6b93375a6ac562e830137491bef4
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-03-20 02:01:59 -0300

    Update views.py

--- utils/views.py
+++ utils/views.py
@@ -4,24 +4,44 @@ from discord.ext import commands
 class CustomDropdown(discord.ui.Select):
     def __init__(self):
 
-        # Set the options that will be presented inside the dropdown
         options = [
-            discord.SelectOption(label='Utility', emoji='🟥'),
-            discord.SelectOption(label='Green', description='Your favourite colour is green', emoji='🟩'),
-            discord.SelectOption(label='Blue', description='Your favourite colour is blue', emoji='🟦'),
+            discord.SelectOption(label='Utility'),
+            discord.SelectOption(label='Information'),
+            discord.SelectOption(label='Moderation'),
         ]
 
-        super().__init__(placeholder='Choose your favourite colour...', min_values=1, max_values=1, options=options)
+        super().__init__(placeholder='Select a category', min_values=1, max_values=1, options=options)
     
     async def callback(self, interaction: discord.Interaction):
+        VALUE = interaction.data['values'][0]
+        print(VALUE)
+        if VALUE =="Utility":
+            embed = discord.Embed(colour=1399944)
+            embed.set_author(name="Util commands")
+            embed.description = "**</help:0> -> Help command \n </reverse:0> -> Reverse a text \n </say:0> -> I repeat your text**"
+            await interaction.response.edit_message(embed=embed)
+
+        elif VALUE =="Information":
+                embed = discord.Embed(colour=1399944)
+                embed.title = "Information commands"
+                embed.description = "**</userinfo:0> -> Get a user information \n </serverinfo:0> -> Get the server information \n </channel:0> -> Get a channel information \n </role:0> -> Get a role information \n </botinfo:0> -> Get the bot information \n </ping:0> -> Show my ping \n </avatar:0> -> Get a user avatar \n </icon:0> -> Get the server icon**"
+                await interaction.response.edit_message(embed=embed)
+
+        elif VALUE=="Moderation":
+            try:
+                embed = discord.Embed(colour=1399944)
+                embed.title = "Moderation commands"
+                embed.description = "**</ban:0> -> Ban a user from the guild \n </unban:0> -> Unban a user from the guild \n </kick:0> -> Kick a user from the guild \n </lock:0> -> Lock a channel \n </unlock:0> -> Unlock a channel \n </prefix:0> -> Set a new prefix in this server**"
+                await interaction.response.edit_message(embed=embed)
+            except Exception as err:
+                print(err)
         try:
-            await interaction.response.edit_message(content=f'Your favourite colour is {self.values[0]}')
+                pass
         except Exception as err:
-            print(err)
+                print(err)
 
 class DropdownView(discord.ui.View):
     def __init__(self):
         super().__init__()
 
-        # Adds the dropdown to our view object.
         self.add_item(CustomDropdown())
