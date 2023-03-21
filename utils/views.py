c1036fa9dcf863b83bcbeacef814cd677ec10e70
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-03-21 20:44:09 -0300

    Update views.py

--- utils/views.py
+++ utils/views.py
@@ -3,11 +3,10 @@ from discord.ext import commands
 
 class CustomDropdown(discord.ui.Select):
     def __init__(self):
-
         options = [
-            discord.SelectOption(label='Utility'),
-            discord.SelectOption(label='Information'),
-            discord.SelectOption(label='Moderation'),
+            discord.SelectOption(label='Utility', description='Get util commands'),
+            discord.SelectOption(label='Information', description='Get information commands'),
+            discord.SelectOption(label='Moderation', description='Get moderation commands'),
         ]
 
         super().__init__(placeholder='Select a category', min_values=1, max_values=1, options=options)
@@ -16,22 +15,22 @@ class CustomDropdown(discord.ui.Select):
         VALUE = interaction.data['values'][0]
         print(VALUE)
         if VALUE =="Utility":
-            embed = discord.Embed(colour=1399944)
+            embed = discord.Embed(colour=14474460)
             embed.set_author(name="Util commands")
-            embed.description = "**</help:0> -> Help command \n </reverse:0> -> Reverse a text \n </say:0> -> I repeat your text**"
+            embed.description = "> **Total: 3 commands | Prefix and Slash** \n\n **Command                   Description \n </help:0>                            Commands list \n </say:0>                              I repetar your text \n </reverse:0>                      Reverse your text \n </role:0>                             Get a role information **"
             await interaction.response.edit_message(embed=embed)
 
         elif VALUE =="Information":
-                embed = discord.Embed(colour=1399944)
-                embed.title = "Information commands"
-                embed.description = "**</userinfo:0> -> Get a user information \n </serverinfo:0> -> Get the server information \n </channel:0> -> Get a channel information \n </role:0> -> Get a role information \n </botinfo:0> -> Get the bot information \n </ping:0> -> Show my ping \n </avatar:0> -> Get a user avatar \n </icon:0> -> Get the server icon**"
+                embed = discord.Embed(colour=14474460)
+                embed.set_author(name="Information commands")
+                embed.description = "> **Total: 8 commands | Prefix & Slash** \n\n**Command                   Description \n </userinfo:0>                    Get a user information \n </serverinfo:0>                 Get the server information \n </botinfo:0>                      Get the bot info \n </icon:0>                            Get the server icon \n </ping:0>                            Show my ping \n </channel:0>                     Get a channel information \n </avatar:0>                        Get a user avatar**"
                 await interaction.response.edit_message(embed=embed)
 
         elif VALUE=="Moderation":
             try:
-                embed = discord.Embed(colour=1399944)
-                embed.title = "Moderation commands"
-                embed.description = "**</ban:0> -> Ban a user from the guild \n </unban:0> -> Unban a user from the guild \n </kick:0> -> Kick a user from the guild \n </lock:0> -> Lock a channel \n </unlock:0> -> Unlock a channel \n </prefix:0> -> Set a new prefix in this server**"
+                embed = discord.Embed(colour=14474460)
+                embed.set_author(name="Moderation commands")
+                embed.description = "> **Total: 6 commands | Prefix & Slash** \n\n**Command                   Description \n</ban:0>                              Ban a user form this guild \n </unban:0>                         Unban a user from this guild \n </kick:0>                             Kick a user from this guild \n </lock:0>                             Lock a channel \n </unlock:0>                         Unlock a channel \n </prefix:0>                          Set a new prefix in this guild**"
                 await interaction.response.edit_message(embed=embed)
             except Exception as err:
                 print(err)
