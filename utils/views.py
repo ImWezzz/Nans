659fd834d3c68fffbf90d762e5d75f3c56e2f96c
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-03-28 01:40:50 -0300

    Update views.py

--- utils/views.py
+++ utils/views.py
@@ -17,20 +17,20 @@ class CustomDropdown(discord.ui.Select):
         if VALUE =="Utility":
             embed = discord.Embed(colour=14474460)
             embed.set_author(name="Util commands")
-            embed.description = "> **Total: 3 commands | Prefix and Slash** \n\n **Command                   Description \n </help:0>                            Commands list \n </say:0>                              I repetar your text \n </reverse:0>                      Reverse your text \n </role:0>                             Get a role information **"
+            embed.description = "> **Total: 3 commands | Prefix and Slash** \n\n **Command ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Description \n </help:0>‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Commands list \n </say:0>‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎  ‎ I repetar your text \n </reverse:0>‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Reverse your text**"
             await interaction.response.edit_message(embed=embed)
 
         elif VALUE =="Information":
                 embed = discord.Embed(colour=14474460)
                 embed.set_author(name="Information commands")
-                embed.description = "> **Total: 8 commands | Prefix & Slash** \n\n**Command                   Description \n </userinfo:0>                    Get a user information \n </serverinfo:0>                 Get the server information \n </botinfo:0>                      Get the bot info \n </icon:0>                            Get the server icon \n </ping:0>                            Show my ping \n </channel:0>                     Get a channel information \n </avatar:0>                        Get a user avatar**"
+                embed.description = "> **Total: 8 commands | Prefix & Slash** \n\n**Command ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Description \n </userinfo:0>‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Get a user information \n </serverinfo:0> ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Get the server information \n </botinfo:0>‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Get the bot info \n </icon:0>‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Get the server icon \n </ping:0>  ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Show my ping \n </channel:0> ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Get a channel information \n </avatar:0> ‎ ‎ ‎ ‎ ‎‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Get a user avatar**"
                 await interaction.response.edit_message(embed=embed)
 
         elif VALUE=="Moderation":
             try:
                 embed = discord.Embed(colour=14474460)
                 embed.set_author(name="Moderation commands")
-                embed.description = "> **Total: 6 commands | Prefix & Slash** \n\n**Command                   Description \n</ban:0>                              Ban a user form this guild \n </unban:0>                         Unban a user from this guild \n </kick:0>                             Kick a user from this guild \n </lock:0>                             Lock a channel \n </unlock:0>                         Unlock a channel \n </prefix:0>                          Set a new prefix in this guild**"
+                embed.description = "> **Total: 6 commands | Prefix & Slash** \n\n**Command** ㅤㅤㅤㅤㅤ**Description \n</ban:0>ㅤㅤㅤㅤㅤㅤㅤㅤBan a user form this guild \n </unban:0>ㅤ ㅤㅤㅤㅤㅤㅤUnban a user from this guild \n </kick:0>ㅤㅤㅤㅤㅤㅤㅤㅤKick a user from this guild \n </lock:0>ㅤㅤㅤㅤㅤㅤㅤㅤLock a channel \n </unlock:0>ㅤㅤㅤㅤㅤㅤㅤUnlock a channel \n </prefix:0>ㅤㅤㅤㅤㅤㅤㅤSet a new prefix in this guild**"
                 await interaction.response.edit_message(embed=embed)
             except Exception as err:
                 print(err)
